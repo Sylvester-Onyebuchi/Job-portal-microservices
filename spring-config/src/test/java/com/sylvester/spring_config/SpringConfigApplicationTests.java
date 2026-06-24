009517c9437bfcf306c0bc12d6f2df472b1253e1
Sylvester Onah <onahsylvester3@gmail.com>
2026-06-24 03:08:37 +0200

    updated ci pipeline files for each service and enabled encryption in springconfig service

--- spring-config/src/test/java/com/sylvester/spring_config/SpringConfigApplicationTests.java
+++ spring-config/src/test/java/com/sylvester/spring_config/SpringConfigApplicationTests.java
@@ -1,13 +1,9 @@
 package com.sylvester.spring_config;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+
 
 }
